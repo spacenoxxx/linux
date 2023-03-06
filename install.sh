9efb247dc4fcd50c8afed78a975f0ce23be517d7
spacenoxxx <72987415+spacenoxxx@users.noreply.github.com>
2023-03-06 14:35:08 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,3 @@
-mkdir -p ~/github \
-cd ~/github
-git clone 
 cd /tmp && wget https://www.c-nergy.be/downloads/xRDP/xrdp-installer-1.4.6.zip
 unzip unzip xrdp-installer-1.4.6.zip 
 chmod +x  ./xrdp-installer-1.4.6.sh
