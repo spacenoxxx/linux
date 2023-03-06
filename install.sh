0d7b8e006aa104e96bb189076d505f6ca65e7f1f
spacenoxxx <72987415+spacenoxxx@users.noreply.github.com>
2023-03-06 14:34:23 +0530

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,8 @@
+mkdir -p ~/github \
+cd ~/github
+git clone 
+cd /tmp && wget https://www.c-nergy.be/downloads/xRDP/xrdp-installer-1.4.6.zip
+unzip unzip xrdp-installer-1.4.6.zip 
+chmod +x  ./xrdp-installer-1.4.6.sh
+sudo apt remove pipewire -y && sudo reboot
+./xrdp-installer-1.4.6.sh -s -l
