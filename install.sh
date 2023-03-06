aab7c5f48c69123c92c3dc553b01ae2c44b17070
spacenoxxx <72987415+spacenoxxx@users.noreply.github.com>
2023-03-06 14:43:13 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 cd /tmp && wget https://www.c-nergy.be/downloads/xRDP/xrdp-installer-1.4.6.zip
-unzip unzip xrdp-installer-1.4.6.zip 
+unzip xrdp-installer-1.4.6.zip 
 chmod +x  ./xrdp-installer-1.4.6.sh
 sudo apt remove pipewire -y && sudo reboot
 ./xrdp-installer-1.4.6.sh -s -l
